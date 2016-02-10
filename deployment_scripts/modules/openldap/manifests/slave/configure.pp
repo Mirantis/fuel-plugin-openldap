a3c7f80b4e40ef04fbca16bc189397168689fb6a
Ukov Dmitry <dukov@mirantis.com>
2016-02-10 16:23:34 +0300

    Added LDAP chaching proxy mode
    Change-Id: I89aad387cfad7540aedcea7f408e537446b253ea

--- deployment_scripts/modules/openldap/manifests/slave/configure.pp
+++ deployment_scripts/modules/openldap/manifests/slave/configure.pp
@@ -2,6 +2,18 @@ class openldap::slave::configure (
   $basedn=undef,
   $master_fqdn=undef,
   $ldap_user_password=undef,
+  $mode = undef,
+  $use_tls = undef,   
+  $hdb_index_params = undef,
+  $cache_suffix = undef,
+  $suffix_massage = undef,
+  $binddn = undef,
+  $bind_password = undef,
+  $cache_attrs = undef,
+  $chahe_index_params = undef,
+  $ldap_cache = false,
+  $upstream_host = undef,
+  $upstream_proto = 'ldap',
   ){
     notice('MODULAR: openldap/slave/configure.pp')
     package { 'supervisor':
@@ -29,16 +41,32 @@ class openldap::slave::configure (
           group   => 'openldap',
           mode    => '0640',
           content => template("openldap/slave-slapd.conf.tmpl.erb"),
+          notify  => Service['supervisor']
     }
-    ->
+    if $ldap_cache {
+      file {'/var/lib/ldap/ldap-cache':
+        ensure => directory,
+	      owner  => 'openldap',
+	      group  => 'openldap',
+	      notify  => Exec['slapindex'],
+	      require => Package['slapd'],
+      }
+
+      exec { 'slapindex':
+	      command => '/usr/sbin/slapindex -v -f /etc/ldap/slapd.conf',
+	      user => 'openldap',
+        before => Service['supervisor'],
+        subscribe => File['/etc/ldap/slapd.conf'],
+      }
+    }
+
     file {"/etc/supervisor/conf.d/slapd-slave.conf":
       path    => "/etc/supervisor/conf.d/slapd-slave.conf",
       owner   => 'root',
       group   => 'root',
       mode    => '0644',
       content => template("openldap/supervisor-slapd-slave.tmpl.erb"),
-    }
-    ~>
+    } ~>
     service { 'supervisor':
       provider => debian,
       ensure => running,
--- deployment_scripts/openldap_slave.pp
+++ deployment_scripts/openldap_slave.pp
@@ -1,1 +1,66 @@
-class { 'openldap::openldap_slave': }
+#class { 'openldap::openldap_slave': }
+notice('MODULAR: openldap/openldap_slave.pp')
+
+
+$plugin_settings     = hiera('openldap')
+$mode                = $plugin_settings['ol-mode']
+$domain_name         = $plugin_settings['ol-domain_name']
+$ldap_user_password  = $plugin_settings['ol-user_password']
+$use_tls             = $plugin_settings['ol-tls']
+$hdb_index           = split($plugin_settings['ol-hdb_index'], '\n')
+$cache_suffix        = $plugin_settings['ol-cache_suffix']
+$suffix_massage      = $plugin_settings['ol-suffixmassage']
+$binddn              = $plugin_settings['ol-binddn']
+$bind_password       = $plugin_settings['ol-bind_password']
+$cache_attrs         = split($plugin_settings['ol-cache_attrs'], '\n')
+$cache_index         = split($plugin_settings['ol-cache_index_params'], '\n')
+$upstream_host       = $plugin_settings['ol-upstream_host']
+$upstream_proto      = $plugin_settings['ol-upstream_proto']
+$ldap_cache          = $mode ? {
+                         'caching_proxy' => true,
+                         default         => false,
+                       } 
+
+if !$ldap_cache {
+  $nodes_hash          = hiera('nodes_hash')
+  $openldap_master     = filter_nodes($nodes_hash, 'role', 'primary-openldap')
+  $master_fqdn         = $openldap_master[0]['fqdn']
+}
+
+$ca_cert     = $plugin_settings['ol-ca_cert']
+$cacrt       = split($ca_cert, ' ')
+$slapd_cert  = $plugin_settings['ol-slapd_cert']
+$slapdcert   = split($slapd_cert, ' ')
+$slapd_key   = $plugin_settings['ol-slapd_key']
+$slapdkey    = split($slapd_key, ' ')
+
+$domain_array  = split($domain_name, '[.]')
+$basedn_tmp    = join( $domain_array, ',dc=')
+$basedn        = "dc=${basedn_tmp}"
+
+class { 'openldap::firewall': port => '389', } ->
+class { 'openldap::logging': } ->
+class { 'openldap::install':
+  ldap_user_password  => $ldap_user_password,
+  domain_name         => $domain_name,
+} ->
+class { 'openldap::slave::cacert':
+  cacert       => $cacrt,
+} ->
+class { 'openldap::slave::configure':
+  basedn             => $basedn,
+  master_fqdn        => $master_fqdn,
+  ldap_user_password => $ldap_user_password,
+  mode               => $mode,
+  use_tls            => $use_tls,   
+  hdb_index_params   => $hdb_index,
+  cache_suffix       => $cache_suffix,
+  suffix_massage     => $suffix_massage,
+  binddn             => $binddn,
+  bind_password      => $bind_password,
+  cache_attrs        => $cache_attrs,
+  chahe_index_params => $cache_index,
+  upstream_host      => $upstream_host,
+  upstream_proto     => $upstream_proto,
+  ldap_cache         => $ldap_cache,
+}
